7535d218fa2b1d639b62d21a3d6221d9e7ba1edd
CCL <anicias@yahoo.com>
2025-01-14 13:21:28 -0500

    more revisions agin

--- .script.js
+++ .script.js
@@ -1,3 +1,15 @@
+
+// Function to be triggered when "Customize Your Cake Now" is clicked
+function customizeCake() {
+    alert("Button 'Customize Your Cake Now' clicked!");
+}
+
+// Function to be triggered when "Send Message for Consultation" is clicked
+function contactUs() {
+    alert("Button 'Send Message for Consultation' clicked!");
+}
+
+
 function customizeCake() {
 alert("Take your time and create your perfect cake with me! Let's start customizing!");
 }
@@ -5,3 +17,4 @@ alert("Take your time and create your perfect cake with me! Let's start customiz
 function contactUs() {
     alert("Thank you for reaching out! I will get back to you soon for a consultation.");
 }
+
