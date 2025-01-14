e96d4ac051d2b7f038faf2ea0928c9ea33d0c11f
CCL <anicias@yahoo.com>
2025-01-14 11:27:27 -0500

    revisions to files

---
+++ .script.js
@@ -0,0 +1,7 @@
+function customizeCake() {
+    alert("Take your time and create your perfect cake with me! Let's start customizing!");
+}
+
+function contactUs() {
+    alert("Thank you for reaching out! I will get back to you soon for a consultation.");
+}
