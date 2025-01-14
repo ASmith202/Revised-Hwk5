a0259d47b6c21da743481e28576c5598d417690a
CCL <anicias@yahoo.com>
2025-01-14 13:26:09 -0500

    fix the issue

--- .script.js
+++ .script.js
@@ -1,20 +1,11 @@
-
 // Function to be triggered when "Customize Your Cake Now" is clicked
 function customizeCake() {
-    alert("Button 'Customize Your Cake Now' clicked!");
+    console.log("Customize button clicked");  // Logs to the console
+    alert("Button 'Customize Your Cake Now' clicked!");  // Shows an alert
 }
 
 // Function to be triggered when "Send Message for Consultation" is clicked
 function contactUs() {
-    alert("Button 'Send Message for Consultation' clicked!");
+    console.log("Contact Us button clicked");  // Logs to the console
+    alert("Button 'Send Message for Consultation' clicked!");  // Shows an alert
 }
-
-
-function customizeCake() {
-alert("Take your time and create your perfect cake with me! Let's start customizing!");
-}
-
-function contactUs() {
-    alert("Thank you for reaching out! I will get back to you soon for a consultation.");
-}
-
