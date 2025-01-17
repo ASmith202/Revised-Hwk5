5665c2b5148219994c7cd975d62c08a4cd648b38
CCL <anicias@yahoo.com>
2025-01-16 20:43:59 -0500

    changes

--- .script.js
+++ .script.js
@@ -1,11 +1,11 @@
-// Function to be triggered when "Customize Your Cake Now" is clicked
 function customizeCake() {
-    console.log("Customize button clicked");  // Logs to the console
-    alert("Button 'Customize Your Cake Now' clicked!");  // Shows an alert
+    console.log("Customize button clicked");  
+    alert("Button 'Customize Your Cake Now' clicked!"); 
 }
 
-// Function to be triggered when "Send Message for Consultation" is clicked
 function contactUs() {
-    console.log("Contact Us button clicked");  // Logs to the console
-    alert("Button 'Send Message for Consultation' clicked!");  // Shows an alert
+    console.log("Contact Us button clicked");  
+    alert("Button 'Send Message for Consultation' clicked!");  
 }
+
+
